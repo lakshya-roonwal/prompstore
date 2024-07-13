901d57d909c5ec4a897ed523ebd9875ac0151bd0
lakshya-roonwal <erenpool7214@gmail.com>
2024-07-13 13:03:22 +0530

    feat : Loading state of butotn

--- src/components/PromptSearcher.tsx
+++ src/components/PromptSearcher.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { Input } from "@/components/ui/input";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import Spinner from "./Spinner";
 
 interface Prompt {
   _id: string;
@@ -59,12 +60,14 @@ const PromptSearcher = () => {
             onClick={(e)=>{
                 searchQuery.length>2?handleSearch(e):null;
             }}
+            disabled={isLoading}
             className="bg-primary text-primary-foreground rounded-md px-4 py-2 font-medium hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
           >
-            {isLoading ? "Searching..." : "Search"}
+            {isLoading ? <>Search <Spinner/></> : "Search "}
           </Button>
         </form>
         <div className="space-y-4">
+          {prompts.length===0&&<h1>Search some prompts</h1>}
           {prompts.map((prompt) => (
             <div
               key={prompt._id}
