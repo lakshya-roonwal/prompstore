f4d969663b2e044575f64030a660b4983a1bc97d
lakshya-roonwal <erenpool7214@gmail.com>
2024-07-13 13:09:35 +0530

    feat : Getting the latest prompts

--- src/app/api/getprompts/route.ts
+++ src/app/api/getprompts/route.ts
@@ -7,7 +7,7 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const searchPrompt=request.nextUrl.searchParams.get("prompt") as string;
-    const prompts = await Prompts.find({$text:{$search:searchPrompt}}).sort([['created_at', 1]]).limit(30);
+    const prompts = await Prompts.find({$text:{$search:searchPrompt}}).sort([['created_at', -1]]).limit(30);
     return NextResponse.json({ msg: "Sucess",prompts }, { status: 200 });
     
   } catch (error) {
